4153026479a52e0cf07121a1e13cbb7dcf45c377
ringtail003 <ringtail003@gmail.com>
2020-11-22 18:27:35 +0900

    npm run start:e2eでE2Eテスト環境を立ち上げる

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { LoginComponent } from "src/app/components/login/login.component";
 import { ProfileComponent } from "src/app/components/profile/profile.component";
+import { MockWebApi } from "src/app/mocks/mock-web-api";
+import { environment } from "src/environments/environment";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
@@ -18,6 +21,7 @@ import { AppComponent } from "./app.component";
     HttpClientModule,
     CommonModule,
     RouterModule,
+    environment.e2e ? HttpClientInMemoryWebApiModule.forRoot(MockWebApi) : [],
   ],
   providers: [],
   bootstrap: [AppComponent],
